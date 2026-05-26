ff32dae117173c81d1cc1ed5d194810e6cc70c35
unknown <jose22mendez22@gmail.com>
2026-05-26 12:08:32 -0700

    Refactored todolist to have dicts, updated persistance and added status handeling

--- TodoList.py
+++ TodoList.py
@@ -1,38 +1,45 @@
 import json
+
 fileName = 'todo_list.json'
-todoList = []
 
 try:
     with open(fileName, 'r') as f:
-        if f.read(1):
-            f.seek(0)
-            todoList = json.load(f)
-        else:
-            print('File Empty') 
-except FileNotFoundError:
-    with open(fileName, 'w') as f:
-        f.write('')
+        todoList = json.load(f)
+except (FileNotFoundError, json.JSONDecodeError):
+    todoList = []
 
 
 def printTodoList():
-    for i in todoList:
-        print(i)
+    for todo in todoList:
+        status = ''
+        if todo['completed'] == False:
+            status = 'Not done'
+        else:
+            status = 'Done'
+        print(todo['todo'] + ' : ' + status)
 
 print('Your TODO List:')
 printTodoList()
 
 while True:
-    print('Type (0) to exit')
+    print('Type (0) to Exit')
     print('Type (1) to Add a TODO')
-    print('Type (2) to Remove a TODO')
+    print('Type (2) to Update TODO')
+    print('Type (3) to Remove a TODO')
     userInput = input()
     if userInput == '1':
         todo = input('Enter your new TODO: ')
+        todo = { 'todo' : todo, 'completed' : False }
         todoList.append(todo)
         printTodoList()
     elif userInput == '2':
-        todoLocation = input('Enter number of todo in list order: ')
-        todoList.pop(int(todoLocation) - 1)
+        index = input('Enter number of todo in list order: ')
+        index = int(index) - 1
+        todoList[index]['completed'] = not todoList[index]['completed']
+        printTodoList()
+    elif userInput == '3':
+        index = input('Enter number of todo in list order: ')
+        todoList.pop(int(index) - 1)
         printTodoList()
     elif userInput == '0':
         break
